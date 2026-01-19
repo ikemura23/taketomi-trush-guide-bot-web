3e8751ceae58943f4c41420b480274e320307887
ikemura23 <banbara.work@gmail.com>
2026-01-19 10:57:36 +0900

    feat: App に Noticeコンポーネントを表示

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero'
 import Features from './components/Features'
 import HowToUse from './components/HowToUse'
 import Transparency from './components/Transparency'
+import Notice from './components/Notice'
 import Footer from './components/Footer'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="min-h-screen bg-white">
       <Hero />
       <Features />
+      <Notice />
       <HowToUse />
       <Transparency />
       <Footer />
